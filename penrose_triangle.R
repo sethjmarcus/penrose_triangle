a529db8cba17c995e6c8a6358a201f305af0c67d
Seth Marcus <seth.marcus07@qmail.cuny.edu>
2024-03-03 19:09:42 -0500

    First commit

---
+++ penrose_triangle.R
@@ -0,0 +1,12 @@
+plot(515,500, col = NA, xlab = NA, ylab = NA, main="Penrose Triangle")
+
+
+polygon(x = c(480, 635, 465, 435, 735, 550),
+        y = c(298, 578, 578, 633, 633, 298),
+        col = "blue")
+polygon(x = c(735, 435, 520, 485, 330, 705),
+        y = c(633, 633, 483, 418, 698, 698),
+        col = "yellow")
+polygon(x = c(330, 485, 570, 635, 480, 295),
+        y = c(698, 418, 578, 578, 298, 638),
+        col = "green")
